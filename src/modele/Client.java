2d13999e05e04291a0641028ce09585863908162
eliott.gaudillat <eliott.gaudillat@univ-tlse3.fr>
2022-02-17 18:26:44 +0100

    TP2 CarteBancaire OK

--- src/modele/Client.java
+++ src/modele/Client.java
@@ -2,14 +2,22 @@ package modele;
 
 public class Client extends Profil{
 
+    private CarteBancaire carteBancaire;
+
     public Client(String nom, String prenom, String mdp){
         super(nom,prenom,mdp);
+    }
+
+    public void enregistrerCoordonneesBancaires(int numeroCarte,int dateCarte){
+        this.carteBancaire = new CarteBancaire(numeroCarte,dateCarte);
     }
 
     @Override
     public String toString() {
         return "Client["+
                 super.toString()+
+                ",carteBancaire ="+
+                carteBancaire.toString()+
                 "]";
     }
 }
